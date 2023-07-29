b383f4cbe88d211e320d39be7852413b83f4291e
EliteTeamDev <131678199+EliteTeamDev@users.noreply.github.com>
2023-07-29 11:14:39 +0200

    added dynamic page loading

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='home'),
 
     # Matches any html file
-    # re_path(r'^.*\.*', views.pages, name='pages'),
+    re_path(r'^.*\.*', views.pages, name='pages'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django import template
 # Create your views here.
 
 def index(request):
@@ -9,3 +9,28 @@ def index(request):
 
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
+
+
+def pages(request):
+    context = {'stage': 1}
+    # All resource paths end in .html
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]  # picks the url of the template to be loaded
+
+        context['segment'] = load_template
+        html_template = loader.get_template('home/' + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    # ##################// The above is the code that loads any template to the front end //############################
+    # ##################################################################################################################
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('home/page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template('home/page-500.html')
+        return HttpResponse(html_template.render(context, request))
