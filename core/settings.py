55c332dd1403c6130a4ffe7738ac7908d0d06ea2
Spencer Tasa <90764312+GhostFulltech@users.noreply.github.com>
2023-08-29 07:16:56 +0200

    project life changing updates

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.authentication',
-    'apps.home'
+    'apps.home',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
